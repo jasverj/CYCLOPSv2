a7a645283e7e3e21b34c6270fad9016c6e66547f
jasverj <jasverj@gmail.com>
2019-12-03 20:04:24 -0500

    Basic Data Processing Functions
    Added functions for basic data processing. This includes finding "NA" times in list of times, converting a DataFrame or Array{Any} containing strings that can be converted to Float32 to Array{Float32}, finding expression data for a list of relevant genes that have a minimum average expression and a coefficient of variation within a given range.

--- CYCLOPS_DP_DM.jl
+++ CYCLOPS_DP_DM.jl
@@ -1,49 +1,164 @@
 using DataFrames, Statistics, LinearAlgebra, StatsBase, MultivariateStats, Distributions, CSV, Juno
 
-#########################################################
-# Convert Array{Any} | Array{String} to Array{Float32} #
-#######################################################
-# Array{Any} | Array{String} to Array{Flaot32}  #
-################################################
-# All strings must be convertible to numbers  #
+#------------------#
+# Data Processing #
+#----------------#
+
+#####################
+# begin makefloat! #
+#############################################################
+# Convert Array{Any} | Array{String} to Array{Float32}     #
+###########################################################
+# Method 1: Array{Any} | Array{String} to Array{Flaot32} #
+#-------------------------------------------------------#
+# All strings must be convertible to numbers           #
+#-----------------------------------------------------#
 function makefloat!(ar::Array{Any}) # convert to Array{Any} first, using convert{Matrix, df}.
-    for col in 1:size(ar)[2]
-        for row in 1:size(ar)[1]
-            if typeof(ar[row,col]) == String
-                ar[row, col] = parse(Float32, ar[row,col])
-            end
-        end
-    end
-    ar = convert(Array{Float32, 2}, ar)
-end
+    for col in 1:size(ar)[2] # size[2] is the number of columns
+        for row in 1:size(ar)[1] # size[1] is the number of rows
+            if typeof(ar[row,col]) == String # if the type of a field is string...
+                ar[row, col] = parse(Float32, ar[row,col]) # ...that field will be converted to Float32
+            end #--end if--#
+        end #--end for--#
+    end #--end for--#
 
-# Array{Any,1} | Array{String,1} to Array{Float32}      #
-########################################################
-# Will additionally convert from row to column vector #
-function makefloat!(ar::Array{Any,1}, conv::Bool=false)
-    for col in 1:size(ar)[1]
-        if typeof(ar[col]) == String
-            ar[i] = parse(Float32, ar[i])
-        end
-        if conv == true
-            convert(Array{Float32,2}, ar')
-        end
-    end
+    ar = convert(Array{Float32, 2}, ar) # Now that all fields are type Float32 the Array{Any} can be converted to Array{Float32}
 end
+#--end function--#
 
-# DataFrame to Array{Float32}               #
-############################################
+#--------------------------------------------------------------------#
+# Method 2/3: Array{Any,1} | Array{String,1} to Array{Float32}      #
+#------------------------------------------------------------------#
+# Will additionally convert from row to column vector             #
+#----------------------------------------------------------------#
+function makefloat!(ar::Array{Any,1}, flip::Bool=false)
+    for row in 1:size(ar)[1] # size[1] is the number of rows
+        if typeof(ar[row]) == String # if the type of a field is string...
+            ar[i] = parse(Float32, ar[i]) # ...that field will be converted to Float32
+        end #--end if--#
+        if flip == true # if flip is true...
+            ar = convert(Array{Float32,2}, ar') # ...the array is converted from a row vector to a column vector...
+		else # ...otherwise if flip is false...
+			ar = convert(Array{Float32,1}, ar) # ...just return Array{Float32,1}
+        end #--end if--#
+    end #--end for--#
+
+	ar # return ar
+end
+#--end function--#
+
+#--------------------------------------------#
+# Method 4: DataFrame to Array{Float32}     #
+#------------------------------------------#
 # goes directly from df to Array{Float32} #
+#----------------------------------------#
 function makefloat!(df::DataFrame) # will convert to Array{Float} first
-    ar = convert(Matrix, df)
-    for col in 1:size(ar)[2]
-        for row in 1:size(ar)[1]
-            if typeof(ar[row,col]) == String
-                ar[row, col] = parse(Float32, ar[row,col])
-            end
-        end
-    end
-    ar = convert(Array{Float32, 2}, ar)
+    ar = convert(Matrix, df) # Fist convert the DataFrame to a matrix. If strings are contained it will become an Array{Any}
+    for col in 1:size(ar)[2] # For each column
+        for row in 1:size(ar)[1] # For each row
+            if typeof(ar[row,col]) == String # Check if the element is a string...
+                ar[row, col] = parse(Float32, ar[row,col]) # ...and convert it to Float32
+            end #--end if--#
+        end #--end for--#
+    end #--end for--#
+
+    ar = convert(Array{Float32, 2}, ar) # Now that all elements are type Float32 the Array{Any} can be converted to an Array{Float32}
 end
+#--end function--#
+###################
+# end makefloat! #
+#################
+
+
+
+#####################
+# begin findNAtime #
+#################################################################
+# Find indices of fields containting string NA and return list #
+###############################################################
+# Method 1: find strings in DataFrame                        #
+#-----------------------------------------------------------#
+function findNAtime(df)
+    r = [] # initialize vector for storing indeces
+        for ii in 1:length(df) # go through each element in the DataFrame...
+            if typeof(df[ii]) == String # ...check if is type string...
+                append!(r, ii) # ...if so, add the index to the list.
+            end #--end if--#
+        end #--end for--#
+
+    r # return the list containing indeces for elements containing the string NA
+end
+#--end function--#
+###################
+# end findNAtime #
+#################
+
+
+
+######################
+# begin getSeedData #
+######################################################################################
+# Find known genes with minimum average expression and with CV within a given range #
+####################################################################################
+# Method 1: Array{Float32}                                                        #
+#--------------------------------------------------------------------------------#
+function getSeedData(data_data, symbol_list, data_symbols, maxcv, mincv, minmean, bluntpercent)
+	data_data = Array{Float32}(data_data) # convert Array{Float32} to increase speed
+	data_data = cleandata!(data_data, bluntpercent) # remove outliers according to blunt and replace lowest and highest value with new min and max
+
+	gene_means = vec(mean(data_data, dims=2)) # get the means of each row
+	gene_sds = vec(std(data_data; dims=2)) # get the standard deviation of each row
+	gene_cvs = gene_sds ./ gene_means # get the coefficient of variation (CV) -- CV is a good measure of relative variability
+
+	criteria1 = findall(in(symbol_list), data_symbols) # find genes list of known genes in full list of genes from data set
+	criteria2 = findall(gene_means .> minmean) # find all genes with a mean expression level greater than the minimum (user defined) mean
+	criteria3 = findall(gene_cvs .> mincv) # find all genes with a CV greater than the minimum (user defined) CV...
+	criteria4 = findall(gene_cvs .< maxcv) # ...but smaller than the maximum (user defined) CV.
+
+	allcriteria = intersect(criteria1, criteria2, criteria3, criteria4) # find all the genes for which all conditions are true
+	seed_data = data_data[allcriteria, :] # store all samples of genes that meat all the criteria in seed_data
+	seed_symbols = data_symbols[allcriteria, :] # store all the names of the genes being kept in seed_symbols
+
+	seed_symbols, seed_data # return the stored gene names and their respective samples
+end
+#--end function--#
+####################
+# end getSeedData #
+##################
+
+
+
+#####################
+# begin cleandata! #
+###########################################################################################
+# Replace outliers from data according to bluntpercent eith new min and max values       #
+#########################################################################################
+# Method 1: Array{Float32,2}                           								   #
+#-------------------------------------------------------------------------------------#
+function cleandata!(data::Array{Float32, 2}, bluntpercent)
+	ngenes, nsamples = size(data) # number of rows = ngenes, number of columns = nsamples
+	nfloor = Int(1 + floor((1 - bluntpercent) * nsamples)) # index of lowest value to be kept
+	nceiling = Int(ceil(bluntpercent*nsamples)) # index of highest value to be kept
+	for row in 1:ngenes # go through each row
+		sorted = sort(vec(data[row, :])) # sort each row from lowest to highest
+		vfloor = sorted[nfloor] # find the value of the desired new minimum
+		vceil = sorted[nceiling] # find the value of the desired new maximum
+		for sample in 1:nsamples # go through each sample of each row
+			data[row, sample] = max(vfloor, data[row, sample]) # if the value of a given field is lower than the new minimum it will be replaced with the new minimum
+			data[row,sample] = min(vceil, data[row, sample]) # if the value of a given field is higher than the new maximum it will be replaced with the new maximum
+		end #--end for--#
+	end #--end for--#
+
+	data
+end
+#--end function--#
+###################
+# end cleandata! #
+#################
+
+#--------------------#
+# Data Manipulation #
+#------------------#
 
 end
+#--end module--#

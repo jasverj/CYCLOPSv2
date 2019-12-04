28ce193c768437e9af5b6818c7059bbef1b6bc3b
jasverj <jasverj@gmail.com>
2019-12-03 21:21:02 -0500

    Added comment for getEigengenes
    added comments

--- CYCLOPS_DP_DM.jl
+++ CYCLOPS_DP_DM.jl
@@ -26,11 +26,11 @@ function makefloat!(ar::Array{Any}) # convert to Array{Any} first, using convert
 end
 #--end function--#
 
-#--------------------------------------------------------------------#
-# Method 2/3: Array{Any,1} | Array{String,1} to Array{Float32}      #
-#------------------------------------------------------------------#
-# Will additionally convert from row to column vector             #
-#----------------------------------------------------------------#
+#---------------------------------------------------------------#
+# Method 2/3: Array{Any,1} | Array{String,1} to Array{Float32} #
+#-------------------------------------------------------------#
+# Will additionally convert from row to column vector        #
+#-----------------------------------------------------------#
 function makefloat!(ar::Array{Any,1}, flip::Bool=false)
     for row in 1:size(ar)[1] # size[1] is the number of rows
         if typeof(ar[row]) == String # if the type of a field is string...
@@ -76,7 +76,7 @@ end
 #################################################################
 # Find indices of fields containting string NA and return list #
 ###############################################################
-# Method 1: find strings in DataFrame                        #
+# Method 1: DataFrame                                        #
 #-----------------------------------------------------------#
 function findNAtime(df)
     r = [] # initialize vector for storing indeces
@@ -130,15 +130,16 @@ end
 
 ##########################
 # begin removeOutliers! #
-###########################################################################################
-# Replace outliers from data according to bluntpercent eith new min and max values       #
-#########################################################################################
-# Method 1: Array{Float32,2}                                                           #
-#-------------------------------------------------------------------------------------#
-function removeOutliers!(data::Array{Float32, 2}, bluntpercent)
+#####################################################################################
+# Replace outliers from data according to bluntpercent with new min and max values #
+###################################################################################
+# Method 1: Array{Float32,2}                                                     #
+#-------------------------------------------------------------------------------#
+# Max for bluntPercent
+function removeOutliers!(data::Array{Float32, 2}, bluntPercent)
 	ngenes, nsamples = size(data) # number of rows = ngenes, number of columns = nsamples
-	nfloor = Int(1 + floor((1 - bluntpercent) * nsamples)) # index of lowest value to be kept
-	nceiling = Int(ceil(bluntpercent*nsamples)) # index of highest value to be kept
+	nfloor = Int(1 + floor((1 - bluntPercent) * nsamples)) # index of lowest value to be kept
+	nceiling = Int(ceil(bluntPercent*nsamples)) # index of highest value to be kept
 	for row in 1:ngenes # go through each row
 		sorted = sort(vec(data[row, :])) # sort each row from lowest to highest
 		vfloor = sorted[nfloor] # find the value of the desired new minimum
@@ -152,29 +153,40 @@ function removeOutliers!(data::Array{Float32, 2}, bluntpercent)
 	data
 end
 #--end function--#
-###################
+########################
 # end removeOutliers! #
-#################
+######################
 
 
 
 ########################
 # begin getEigengenes #
-####
-# Convert data to SVD space
-function getEigengenes(numeric_data::Array{Float32, 2}, fraction_var::Number, dfrac_var::Number, maxeig::Number=30)
+#####################################################################################################################################################################
+# Convert data to SVD space, keeping eigengenes that contribute a minimum (user defined) amount of variance, up to minimum (user defined) total variance captured  #
+###################################################################################################################################################################
+# Method 1: Array{Float32,2}                                                                                                                                     #
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------#
+# Max for total_fraction_var = 1, reasonable value for total_fraction_var = 0.97; Max for indiv_frac_var = 1, reasonable value for indiv_frac_var = 0.025-0.05 #
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------#
+function getEigengenes(numeric_data::Array{Float32, 2}, total_fraction_var::Number, indiv_frac_var::Number, maxeig::Number=30)
     svd_obj = svd(numeric_data) # convert data to SVD object containing singular values and eigengene expression data in SVD space
     expvar = cumsum(svd_obj.S.^2, dims = 1) / sum(svd_obj.S.^2) # .S are the singular values, sorted in descending order. Find the Fraction variance that an eigengene and each eigengene before it makes up from the total variance.
 
-    ReductionDim1 = 1 + length(expvar[expvar .<= fraction_var]) # how many eigengenes need to be included to have captured the minimum (user specified) variance from the eigengenes (from their singular values)
+    ReductionDim1 = 1 + length(expvar[expvar .<= total_fraction_var]) # how many eigengenes need to be included to have captured the minimum (user specified) variance from the eigengenes (from their singular values)
     vardif = diff(expvar, dims = 1) # find the difference between each added eigengene
-    ReductionDim2 = 1 + length(vardif[vardif .>= dfrac_var]) # which eigengenes contribute the minimum (user specified) variance
-    ReductionDim = min(ReductionDim1, ReductionDim2, maxeig) # The maximum number of eigengenes (user specified) that will be kept is the last criteria. Whichever is the smallest will be used as the number of eigengenes to keep
+    ReductionDim2 = 1 + length(vardif[vardif .>= indiv_frac_var]) # which eigengenes contribute the minimum (user specified) variance
+    ReductionDim = min(ReductionDim1, ReductionDim2, maxeig) # The last criteria is the maximum number of eigengenes (user specified) that will be kept. Whichever is the smallest is the number of eigengenes kept
 
     Transform = svd_obj.V[:, 1:ReductionDim]' # .V is the expression of the eigengenes (in SVD space)
 
-    ReductionDim, Array{Float32,2}(10*Transform) # Return the number of eigengenes kept, and
+    ReductionDim, Array{Float32,2}(10*Transform) # Return the number of eigengenes kept, and the expression of the eigengenes.
 end
+#--end function--#
+######################
+# end getEigengenes #
+####################
+
+
 
 #--------------------#
 # Data Manipulation #
